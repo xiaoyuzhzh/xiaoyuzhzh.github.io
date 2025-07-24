c9c95f297c7517acd3d76ed996dbf4e98b93c772
xiaoyuzhzh <xiaoyuzhzh@outlook.com>
2025-07-24 14:05:02 +0800

    init: project code

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import '@/app/global.css';
+import { Provider } from './provider';
+import { Inter } from 'next/font/google';
+import type { ReactNode } from 'react';
+
+const inter = Inter({
+  subsets: ['latin'],
+});
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en" className={inter.className} suppressHydrationWarning>
+      <body className="flex flex-col min-h-screen">
+        <Provider>{children}</Provider>
+      </body>
+    </html>
+  );
+}

138c76d62ad789b5e3a37985775e5539f5a145ad
xiaoyuzhzh <xiaoyuzhzh@outlook.com>
2025-07-24 17:02:32 +0800

    auto: build and deploy (2025-07-24 17:02:32)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,22 @@
 import '@/app/global.css';
-import { Provider } from './provider';
-import { Inter } from 'next/font/google';
-import type { ReactNode } from 'react';
+import {Provider} from './provider';
+import {Inter} from 'next/font/google';
+import type {ReactNode} from 'react';
 
 const inter = Inter({
-  subsets: ['latin'],
+    subsets: ['latin'],
 });
 
-export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
+export default function Layout({children}: { children: ReactNode }) {
+    return (
+        <html lang="en" className={inter.className} suppressHydrationWarning>
+        <head>
+            <link rel="icon" href="/favicon.svg"/>
+            <title>凌波小碎步</title>
+        </head>
+        <body className="flex flex-col min-h-screen">
         <Provider>{children}</Provider>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
